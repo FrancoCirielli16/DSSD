d578b2187a9da546035a2588c06bd9f7de9a15fe
Franco <francoviscardi5@gmail.com>
2026-09-24 19:11:34 -0300

    cambios

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:8001",
+        target: "http://127.0.0.1:8001", //aca cambio el puerto a 8000 para que apunte al backend
         changeOrigin: true,
       },
     },
